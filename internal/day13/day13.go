ea8bf1c65bc60cce571e0c2b94fc3276c5802a46
Alexander Dyszy <alexander.dyszy@columbusglobal.com>
2024-12-15 15:05:18 +0100

    day 13 (part 2)

--- internal/day13/day13.go
+++ internal/day13/day13.go
@@ -1,19 +1,24 @@
 package day13
 
 import (
+	"errors"
 	"github.com/alxdsz/aoc2024/internal/input"
 	"regexp"
 	"strconv"
 )
 
 type Machine struct {
-	buttonA struct{ x, y int }
-	buttonB struct{ x, y int }
-	prize   struct{ x, y int }
+	buttonA  struct{ x, y int }
+	buttonB  struct{ x, y int }
+	prize    struct{ x, y int }
+	solution *struct{ a, b int }
 }
 
 type Solver struct {
 	machines []Machine
+	maxPress int
+	costA    int
+	costB    int
 }
 
 func parseMachineDataLine(dataLine string) (int, int) {
@@ -39,34 +44,70 @@ func NewSolver(inputPath string) *Solver {
 		}
 		machines = append(machines, machine)
 	}
-	return &Solver{machines}
+	return &Solver{machines: machines, costA: 3, costB: 1}
+}
+
+func solveDirectSubstitution(m Machine) (*struct{ a, b int }, error) {
+	x0, y0 := m.buttonA.x, m.buttonA.y
+	x1, y1 := m.buttonB.x, m.buttonB.y
+	cx, cy := m.prize.x, m.prize.y
+
+	Bdividend := (cy*x0 - cx*y0)
+	Bdivisor := (y1*x0 - y0*x1)
+	if Bdivisor == 0 {
+		return nil, errors.New("zero division for B")
+	} else if Bdividend%Bdivisor != 0 {
+		return nil, errors.New("non integer solution")
+	}
+	B := Bdividend / Bdivisor
+
+	Adividend := (cx - B*x1)
+	Adivisor := x0
+	if Adivisor == 0 {
+		return nil, errors.New("zero division for A")
+	} else if Adividend%Adivisor != 0 {
+		return nil, errors.New("non integer solution")
+	}
+	A := Adividend / Adivisor
+
+	return &struct{ a, b int }{A, B}, nil
+}
+
+func (s *Solver) solve() {
+	for i := range s.machines {
+		if solution, err := solveDirectSubstitution(s.machines[i]); err == nil {
+			s.machines[i].solution = solution
+		}
+	}
+}
+
+func (s *Solver) countTokens() int {
+	tokens := 0
+	for _, machine := range s.machines {
+		if machine.solution == nil {
+			continue
+		}
+		if s.maxPress > 0 && (machine.solution.a > s.maxPress || machine.solution.b > s.maxPress) {
+			continue
+		}
+		tokens += machine.solution.a*s.costA + machine.solution.b*s.costB
+	}
+	return tokens
 }
 
 func (s *Solver) SolvePart1() int {
-	totalTokens := 0
-	for _, m := range s.machines {
-		minTokens := -1
-		for a := 0; a <= 100; a++ {
-			for b := 0; b <= 100; b++ {
-				x := a*m.buttonA.x + b*m.buttonB.x
-				y := a*m.buttonA.y + b*m.buttonB.y
-				if x == m.prize.x && y == m.prize.y {
-					tokens := 3*a + b
-					if minTokens == -1 || tokens < minTokens {
-						minTokens = tokens
-					}
-				}
-			}
-		}
-		if minTokens != -1 {
-			totalTokens += minTokens
-		}
-	}
-	return totalTokens
+	s.maxPress = 100
+	s.solve()
+	return s.countTokens()
 }
 
 func (s *Solver) SolvePart2() int {
-	totalTokens := 0
-	// I wish I had time
-	return totalTokens
+	s.maxPress = -1
+	const offset = 10000000000000
+	for i := range s.machines {
+		s.machines[i].prize.x += offset
+		s.machines[i].prize.y += offset
+	}
+	s.solve()
+	return s.countTokens()
 }
